bfbfe5fb520918b9aed554b71cc8b4fb4fa53f69
Michał Adamczyk <hello@codeman.it>
2020-08-06 13:25:27 +0200

    Updates

---
+++ precache-manifest.09d76d5a62e15d50b1109b3ed14e80f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f9eafed97e96db56288913c170c4ef",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "0317c0c8d7a39e06acb1",
+    "url": "/currency-converter-react/static/css/main.5f2fd5d9.chunk.css"
+  },
+  {
+    "revision": "df18eda2b5fc548341b9",
+    "url": "/currency-converter-react/static/js/2.abfbd1d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.abfbd1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0317c0c8d7a39e06acb1",
+    "url": "/currency-converter-react/static/js/main.fc819acb.chunk.js"
+  },
+  {
+    "revision": "07b2973be230e9c120d7",
+    "url": "/currency-converter-react/static/js/runtime-main.2415008a.js"
+  },
+  {
+    "revision": "b4d4e828203c5f9bc017a97bf562d440",
+    "url": "/currency-converter-react/static/media/money.b4d4e828.jpg"
+  }
+]);
